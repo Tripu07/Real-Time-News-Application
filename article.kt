a48d95360936e38c22bda2b476e7d8e50f710ec2
Tripu07 <87227815+Tripu07@users.noreply.github.com>
2022-01-29 00:01:42 +0530

    Create article.kt

---
+++ article.kt
@@ -0,0 +1,4 @@
+package com.example.application
+import android.accounts.AuthenticatorDescription
+
+data class Article(val title:String,val url:String,val description:String,val urltoImage:String,val author:String)
